32147d37c50b1cb6a1096ec4dbdabb9174ac67c9
Adaendra <maxime.guilbert@neuf.fr>
2021-04-24 10:29:20 -0400

    WIP - Title processor

--- test/tests.js
+++ test/tests.js
@@ -2,10 +2,26 @@ let markdownProcessor = require("../index");
 
 var expect  = require("chai").expect;
 
-describe("Test", () => {
-    it("sub", () => {
-        markdownProcessor.process("test")
-
-        expect(1).to.equal(1);
+describe("Title processors", () => {
+    it("H1", () => {
+        expect(markdownProcessor.process('# Niveau 1')).to.equal('<h1>Niveau 1</h1>');
+    });
+    it("H2", () => {
+        expect(markdownProcessor.process('## Niveau 2')).to.equal('<h2>Niveau 2</h2>');
+    });
+    it("H3", () => {
+        expect(markdownProcessor.process('### Niveau 3')).to.equal('<h3>Niveau 3</h3>');
+    });
+    it("H4", () => {
+        expect(markdownProcessor.process('#### Niveau 4')).to.equal('<h4>Niveau 4</h4>');
+    });
+    it("H5", () => {
+        expect(markdownProcessor.process('##### Niveau 5')).to.equal('<h5>Niveau 5</h5>');
+    });
+    it("H6", () => {
+        expect(markdownProcessor.process('###### Niveau 6')).to.equal('<h6>Niveau 6</h6>');
+    });
+    it("Ignored", () => {
+        expect(markdownProcessor.process('Hello world!')).to.equal('Hello world!');
     });
 });
