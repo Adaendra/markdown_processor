555db315f3b7dbf7af7febcb31a02ce68a89613c
Adaendra <maxime.guilbert@neuf.fr>
2021-04-24 09:24:02 -0400

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 
-module.exports = require('./lib/');
+module.exports = require('./lib');
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,11 @@
+let markdownProcessor = require("../index");
+
 var expect  = require("chai").expect;
 
 describe("Test", () => {
     it("sub", () => {
+        markdownProcessor.process()
+
         expect(1).to.equal(1);
     });
 });
