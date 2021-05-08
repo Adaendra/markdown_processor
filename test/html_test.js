3205bf758966b19a759011807796f125cad219aa
Adaendra <maxime.guilbert@neuf.fr>
2021-05-08 10:00:56 -0400

    Mise en place d'un générateur de fichier HTML avec un cas de test

---
+++ test/html_test.js
@@ -0,0 +1,62 @@
+let htmlProcessor = require("../lib/index").htmlGenerator;
+
+var expect  = require("chai").expect;
+
+describe("HTML Generator", () => {
+    it('Dummy test to generate an html file', () => {
+        htmlProcessor.generateHtmlFile('Test', 'test.html', `
+# Test 1
+## Test 2
+### Test 3
+#### Test 4
+##### Test 5
+###### Test 6
+
+> test
+> **test**
+>> *coucou*
+
+> **[INFO]**
+> Ceci est une info
+
+> **[WARNING]**
+> Ceci est un warning
+
+> **[ALERT]**
+> Ceci est une alerte
+
+~~strike~~
+
+---
+
+- a
++ b
+* c
+- [ ] e
+- [x] e2
+99. az
+1. ez
+7. az
+- e
+5. ee
+
+[Google](https://google.com)
+
+![Overlord{"width":"150px","height":"300px", "showLegend":true}](https://pbs.twimg.com/media/E03ocFyXIAQ4yie?format=jpg&name=large)
+
+\`\`\`javascript
+     let test = pouet;
+     console.log(test);       
+\`\`\`
+
+|a|b|
+|---|---
+|1|2
+|3|4
+
+Tokyo
+: Japan
+: Asia
+        `)
+    });
+});
--- test/test.js
+++
@@ -1,9 +0,0 @@
-let markdownProcessor = require("../lib/index").markdownProcessor;
-
-var expect  = require("chai").expect;
-
-describe("Array processor", () => { // TODO
-    it('Multiline 1', () => {
-        //markdownProcessor.generateHtml()
-    });
-});
